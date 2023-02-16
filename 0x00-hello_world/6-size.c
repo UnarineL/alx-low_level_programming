28f4c334f2644f420969b0f63ca312606e7ffe76
unarine Luvhimbi <unarineluvhimbi8@gmail.com>
2023-02-16 06:59:07 -0800

    Testing 32 and 64 bit data type size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,11 +8,11 @@ int main(void)
 	long int lI;
 	long long int lLI;
 
-	printf("Size of a char: %ld byte(s) \n", (unsigned long)sizeof(c));
-	printf("Size of an int: %ld byte(s) \n", (unsigned long)sizeof(i));
-	printf("Size of a long int: %ld byte(s) \n", (unsigned long)sizeof(lI));
-	printf("Size of a long long int: %ld byte(s) \n", (unsigned long)sizeof(lLI));
-	printf("Size of a float: %ld byte(s) \n", (unsigned long)sizeof(f));
+	printf("Size of a char: %ld byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", (unsigned long)sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", (unsigned long)sizeof(lI));
+	printf("Size of a long long int: %ld byte(s)\n", (unsigned long)sizeof(lLI));
+	printf("Size of a float: %ld byte(s)\n", (unsigned long)sizeof(f));
 
 	return (0);
 }
