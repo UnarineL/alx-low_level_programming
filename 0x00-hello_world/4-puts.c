468ce4165f503b4491d8b016f111cb9e6fe192e0
unarine Luvhimbi <unarineluvhimbi8@gmail.com>
2023-02-16 03:32:31 -0800

    C file returning a string using puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+/*
+ * main -Entry point
+ *
+ *Description: Code returns a string using puts function
+ *
+ * return: Always 0 (success)
+ */
 int main(void)
 {
 	char myString[] = "\"Programming is like building a multilingual puzzle";
