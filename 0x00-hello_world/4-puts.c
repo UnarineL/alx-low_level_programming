4bad250eeb270fc2b122c9bea5ada1200cd7e5a5
unarine Luvhimbi <unarineluvhimbi8@gmail.com>
2023-02-16 02:41:25 -0800

    Using puts func instead of printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 
 int main(void)
 {
-	char myString[] = "\"Programing is like building a multilingual puzzle";
-	
+	char myString[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(myString);
+
 	return (0);
 }
