6e44939f79ba34422ba9fec403598dcab3d8d7c0
unarine Luvhimbi <unarineluvhimbi8@gmail.com>
2023-02-17 06:44:00 -0800

    Print numbers of base 10

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,12 @@ int main(void)
 	for (i = 48; i <= 57; i++)
 	{
 		putchar(i);
-		
+
 		if (i < 57)
 		{
 			putchar(',');
+			putchar(' ');
 		}
-		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
