d661e06741bace870805425bdfa89b732729353d
unarine Luvhimbi <unarineluvhimbi8@gmail.com>
2023-02-17 05:06:03 -0800

    Print alphabets exclude e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabets and skip q and e
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q')
+		{
+			continue;
+		}
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
