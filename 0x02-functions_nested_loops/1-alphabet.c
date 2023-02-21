a4c2173b7d14273c5c7b5c7fa3f31070a92e9028
unarine Luvhimbi <unarineluvhimbi8@gmail.com>
2023-02-21 05:24:53 -0800

    Alphabet function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print alphabet in lower cases
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+}
+
+/**
+* main - Entry point
+*
+* Return: 0 always.
+*/
+int main(void)
+{
+	print_alphabet();
+	putchar('\n');
+
+	return (0);
+}
