83a36370e04e32ce4fe43e8912c5b9a7f29df2f1
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-02-21 12:26:36 -0800

    teste if its lower case or upper case

---
+++ 0x02-functions_nested_loops/3-isLower.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _isLower - returns 1 if alphabet is lowercase otherwise 0
+ * 
+ * Return: 1 if true or 0 if false
+ */
+int _isLower(int c)
+{
+	int lowerCase = 0;
+	int notLowerCase = 1;
+
+	if (c >= 65 && c <= 90)
+	{
+		return (lowerCase);
+	}
+	else if (c >= 97 && c <= 122)
+	{
+		return (notLowerCase);
+	}
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
-int _putchari(char c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _isLower(int c);
 
 #endif
