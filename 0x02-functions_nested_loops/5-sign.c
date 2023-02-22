2d59932d1ed26d952ea1452ce9ebd29ebf2b75a8
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-02-21 23:21:34 -0800

    fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,15 +11,15 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (putchar('+'), 1);
+		return (printf("+\r"), 1);
 	}
 	else if (n < 0)
 	{
-		return (putchar('-'), -1);
+		return (printf("-\r"), -1);
 	}
 	else if (n == 0)
 	{
-		return (putchar('0'), 0);
+		return (printf("0\r"), 0);
 	}
 	return (0);
 }
