88cb1233ee19f9027053d77cca991a4794bae7f5
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-02-22 09:20:54 -0800

    add

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - prints from n to max
+ * @n: takes arg to start from
+ *
+ * Return: value from n to max
+ */
+int print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			printf("%d ", i);
+		}
+	}
+	else
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%d ", i);
+		}
+	}
+}
