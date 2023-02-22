6adb281e882ab626f819fd6bc26b070ad12f558f
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-02-22 03:41:51 -0800

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * times_table - times table of 9
+ */
+void times_table(void)
+{
+    	int row;
+       	int column; 
+	int product;
+
+    	for (row = 0; row <= 9; row++)
+    	{
+        	for (column = 0; column <= 9; column++)
+        	{
+            		product = row * column;
+            		printf("%d,", product);
+        	}
+        	printf("$\n");
+	}
+}	
