f465712c77d1c9e472167e436040aacf8142aaf7
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-02-22 04:20:09 -0800

    changes made

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,11 +11,11 @@ void times_table(void)
 
     	for (row = 0; row <= 9; row++)
     	{
-        	for (column = 0; column <= 9; column++)
-        	{
-            		product = row * column;
+        for (column = 0; column <= 9; column++)
+		{
+           		product = row * column;
             		printf("%d,", product);
-        	}
-        	printf("$\n");
+		}
+		printf("$\n");
 	}
-}	
+}
