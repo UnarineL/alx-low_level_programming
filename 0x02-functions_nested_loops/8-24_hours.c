28d73c0263bc48beb82c3c999b8aefb075614898
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-02-22 03:20:53 -0800

    jack bauer time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_beuer - prints time
+ */
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			printf("%02d:%02d\n", hour, minute);
+		}
+	}
+}
