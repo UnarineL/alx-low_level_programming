f2a4de452f4e20a6715771b4dbf9be1c1d1bf313
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-02-23 04:38:32 -0800

    Print numbers and skip 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * print_most_numbers - Function prints numbers and skip 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			putchar(i + '0');
+		}
+	}
+	putchar('\n');
+}
