4398baa049098ac1a4111c988e07dbd561a5acbb
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-02-23 11:49:14 -0800

    fizz buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Fizz buzz
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 3 == 0) && !(i % 5 == 0))
+		{
+			printf("Fizz ");
+			continue;
+		}
+		else if ((i % 5 == 0) && !(i % 3 == 0))
+		{
+			printf("Buzz ");
+			continue;
+		}
+		else if (i % 5 == 0 && i % 3 == 0)
+		{
+			printf("FizzBuzz ");
+			continue;
+		}
+		printf("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
