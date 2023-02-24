3e26fc8976637f5c5c64c4d950f358969f92851d
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-02-24 05:17:20 -0800

    hello world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+
+/**
+ * main - entry code
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDERR_FILENO, message, strlen(message));
+	return (1);
+}
