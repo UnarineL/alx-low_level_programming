43c6c3b1641b29f4b8d92a88c7d35b5a8155b88f
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-02-24 00:58:01 -0800

    debugging 0-main.c

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * positive_or_negative - checks if number is pos or neg
+ * @i: takes arg to test
+ *
+ * Return: void
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else
+	{
+		printf("%d is negative\n", i);
+	}
+
+}
