4753f922ab5a372e78e287bf7795b1dd46fe72ef
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-02-27 03:45:17 -0800

    swaps value in memory of a and b

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the numbers a and b in memory
+ * @a: takes first arg to be swapped
+ * @b: second arg to be swapped
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 402;
+	*b = 98;
+}
