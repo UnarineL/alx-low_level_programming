15e5c9aed2c3f5547516984ff0136cb3526d7a03
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-02-27 09:05:12 -0800

    fixed odd number

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,17 +9,19 @@
 void puts_half(char *str)
 {
 	int i;
+	int n;
 	int len;
 	int middle;
 
 	len = strlen(str);
 	middle = len / 2;
+	n = len - 2 / 2;
 
 	for (i = middle; i < len; i++)
 	{
 		if (len % 2 != 0)
 		{
-			len = len - 1;		
+			i = n;
 		}
 		putchar(str[i]);
 	}
