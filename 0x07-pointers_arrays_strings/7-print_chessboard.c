4cdfb93b4e103eb86f19b78647b4002f65aa6886
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-03-06 05:23:03 -0800

    function prints chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints a chess board
+ * @a: takes an arg of the array name
+ * 
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		printf("\n");
+	}
+}
