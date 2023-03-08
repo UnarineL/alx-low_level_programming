93e14c9cfff73af9cfd224f44c6712778630688c
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-03-08 01:58:01 -0800

    puts recursion function\

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string source
+ *
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
