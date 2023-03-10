6c13b9482b1c7c504a8e4ba44847327a9db1b688
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-03-10 05:33:33 -0800

    Prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - checks the name of the file
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+adding argc and argv to main
