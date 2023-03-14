c2580f711263490d6fb736fe5d85b8f6216361a1
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-03-14 00:45:40 -0700

    fixed Null error

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,9 +13,9 @@ char *create_array(unsigned int size, char c)
 	char *s;
 
 	i = 0;
-	s = malloc((size + 1) * sizeof(char));
+	s = malloc((size) * sizeof(char));
 
-	if (s == NULL)
+	if (s == 0)
 	{
 		return (NULL);
 	}
