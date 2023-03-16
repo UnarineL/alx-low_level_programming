9cb697203a7fb732f965f701f9bfbe6ef6dba6da
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-03-16 03:00:22 -0700

    allocates mem or array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: an array
+ * @size: size of elements
+ *
+ *
+ * Return: nothing
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	memset(ptr, 0, nmemb * size);
+	return (ptr);
+}
