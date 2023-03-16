68f3e70d3ed4a7b6ae67830957aa598f1b35a59a
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-03-16 05:35:42 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,12 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-void error()
+/**
+ * error - exit function
+ */
+void error(void)
 {
-    printf("Error\n");
-    exit(98);
+	printf("error\n");
+	exit(98);
 }
 
+/**
+ * main - multiplies two positive numbers
+ * @argc: argument counts
+ * @argv: args array
+ *
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 	char *num1;
@@ -15,65 +25,67 @@ int main(int argc, char *argv[])
 	int len1;
 	int len2;
 	int *result;
-	int i, start;
-	int j;
-	
-	
-    	if (argc != 3)
-        	error();
+	int i, j, start;
 
-    	for (i = 1; i < 3; i++)
-    	{
-        	for (j = 0; j < (int)strlen(argv[i]); j++)
-        	{
-            		if (argv[i][j] < '0' || argv[i][j] > '9')
-			{
-                		error();
-			}
-        	}
-    	}
-	
-    	num1 = argv[1];
-    	num2 = argv[2];
-    	len1 = strlen(num1);
-    	len2 = strlen(num2);
-    	result = calloc(len1 + len2, sizeof(int));
-
-   	if (!result)
+	if (argc != 3)
 	{
-        	error();
+		error();
 	}
-    	for (i = len1 - 1; i >= 0; i--)
-    	{
-        	for (j = len2 - 1; j >= 0; j--)
-        	{
-            		int product = (num1[i] - '0') * (num2[j] - '0');
-            		int pos1 = i + j;
-            		int pos2 = i + j + 1;
-            		int sum = product + result[pos2];
-            		result[pos1] += sum / 10;
-            		result[pos2] = sum % 10;
-        	}
-    	}
 
-   	start = 0;
-
-    	while (start < len1 + len2 && result[start] == 0)
-	{	
-		start++;
-	}
-    	if (start == len1 + len2)
-        	printf("0");
-    	else
-    	{
-        	for (i = start; i < len1 + len2; i++)
+	for (i = 1; i < 3; i++)
+	{
+		for (j = 0; j < (int)strlen(argv[i]); j++)
 		{
-			printf("%d", result[i]);
-    	
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				error();
+			}
 		}
 	}
-    	printf("\n");
-    	free(result);
-    	return 0;
-}
 
+	num1 = argv[1];
+	num2 = argv[2];
+
+	len1 = strlen(num1);
+	len2 = strlen(num2);
+	result = calloc(len1 + len2, sizeof(int));
+
+	if (!result)
+	{
+		error();
+	}
+
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			int product = (num1[i] - '0') * (num2[j] - '0');
+			int pos1 = i + j;
+			int pos2 = i + j + 1;
+			int sum = product + result[pos2];
+
+			result[pos1] += sum / 10;
+			result[pos2] = sum % 10;
+		}
+	}
+
+	start = 0;
+	while (start < len1 + len2 && result[start] == 0)
+	{
+		start++;
+	}
+	if (start == len1 + len2)
+	{
+		printf("0");
+	}
+	else
+	{
+		for (i = start; i < len1 + len2; i++)
+		{
+			printf("%d", result[i]);
+		}
+	}
+	printf("\n");
+	free(result);
+	return (0);
+}
