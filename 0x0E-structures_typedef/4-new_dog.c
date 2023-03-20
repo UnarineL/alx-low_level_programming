e09ab6979e693184830e58336a67fd58e49f9564
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-03-20 09:14:04 -0700

    function that creates new_dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -17,8 +17,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 	{
-		free(new_dog->name);
-		free(new_dog->owner);
+
 		free(new_dog);
 		return (NULL);
 	}
@@ -26,6 +25,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->name = malloc(strlen(name) + 1);
 	if (new_dog->name == NULL)
 	{
+		free(new_dog->owner);
 		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
