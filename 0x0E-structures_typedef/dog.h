4ffa46cd4d96d0a8fdffee2e2ae67de855b9441a
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-03-20 06:12:51 -0700

    Dogs struct header file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dog
+ * @name: dog's name
+ * @owner: owner of the dog
+ * @age: dog's age
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
