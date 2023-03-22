697e5b26ae50925ad2c948a9db9da761a64d3b53
Unarine Luvhimbi <unarineluvhimbi@gmail.com>
2023-03-22 08:05:19 -0700

    program that perfoms simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - sum of a and b
+ * @a: first num
+ * @b: sec num
+ *
+ * Return: sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract
+ * @a: first num
+ * @b: sec num
+ *
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - subtract
+ * @a: first nu,
+ * @b: sec num
+ *
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - subtract
+ * @a: first num
+ * @b: sec num
+ *
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - subtract
+ * @a: first num
+ * @b: sec num
+ *
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
