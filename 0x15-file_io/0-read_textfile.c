773973ed788df147f5ffa4387cdbffe14b96a145
Unarine Luvhimbi <unarineluvhimbi8@gmail.com>
2023-07-10 19:42:18 +0200

    read and write file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and print it
+ * @filename: name of the file
+ * @letters: number of letters to read and print
+ *
+ * Return: number of letters to read
+ */
+ssize_t read_textfile(const *filename, size_t letters)
+{
+	int fd;
+	ssize_t r_len, w_len;
+	char *buff;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+	fd = open(filename, O_RDONLY);
+	if (fd == -10)
+	{
+		return (0);
+	}
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	r_len = read(fd, buff, letters);
+	close(fd);
+	if (r_len == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+	w_len = write(STDOUT_FILENO, buffer, r_len);
+	free(buff);
+	if (r_len != w_len)
+	{
+		return (0);
+	}
+	return (w_len);
+}
