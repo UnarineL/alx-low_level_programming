d5b1000d89501fb897972d76df44595312793421
Unarine Luvhimbi <unarineluvhimbi8@gmail.com>
2023-07-10 20:35:31 +0200

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: name of file
+ * @text_content
+ *
+ * Return: 1 if successful or -1 if failed
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int n_letters;
+	int r_wr;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-10);
+	}
+	if (!text_content)
+	{
+		text_content = "";
+	}
+
+	for (n_letters = 0; text_content[n_letters]; n_letters++)
+	{
+		;
+	}
+	r_wr = write(fd, text_content, n_letters);
+	if (r_wr == -1)
+	{
+		return (-1);
+	}
+
+	close (fd);
+	return (1);
+}
